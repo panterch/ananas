7f29ab5925938128214ce29a604467646a70fc06
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2016-05-23 10:06:17 +0200

    style(mentors): use more generic variable names

--- app/controllers/mentors_controller.rb
+++ app/controllers/mentors_controller.rb
@@ -1,8 +1,8 @@
 class MentorsController < CrudController
   def mentor_params
-    mentor_params = params.require(:mentor)
+    permitted_params = params.require(:mentor)
 
-    mentor_params.permit([
+    permitted_params.permit([
       :job_title,
       :who_i_am,
       :experience,
