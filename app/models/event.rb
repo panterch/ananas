d1a772170ce3d7f3023b728a769145a84b5c2306
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2016-05-24 21:42:16 +0200

    style(event): event model cleanup

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,11 +1,10 @@
 class Event < ActiveRecord::Base
-
   belongs_to :mentor
   belongs_to :team
 
   validates_presence_of :summary, :start_at, :end_at
 
   def to_s
-    self.summary
+    summary
   end
 end
