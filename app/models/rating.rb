f541e427dd979bf1c3bd07985c517c48bbea541a
Andreas Brönnimann <bro@panter.ch>
2016-05-26 01:09:23 +0300

    chore(ratings): simplify validation

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -3,19 +3,12 @@ class Rating < ActiveRecord::Base
   belongs_to :event
   belongs_to :mentor
 
-  validate :all_voted
-
   def self.vote_topics
     I18n.t('activerecord.attributes.rating.topic_enum').stringify_keys
   end
 
   vote_topics.keys.each do |vote_topic|
     store_accessor :votes, vote_topic
-  end
-
-  def all_voted
-    votes.each do |topic, rating|
-      errors.add(topic, :blank) if rating.blank?
-    end
+    validates vote_topic, presence: true
   end
 end
