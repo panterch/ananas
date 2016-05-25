9a9c809cc6526fd446de979d73a799623acdee15
Andreas Brönnimann <bro@panter.ch>
2016-05-25 17:19:59 +0300

    feat(rating): Validate vote_topics to be present

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -6,4 +6,9 @@ class Rating < ActiveRecord::Base
   def self.vote_topics
     I18n.t('activerecord.attributes.rating.topic_enum').stringify_keys
   end
+
+  vote_topics.keys.each do |vote_topic|
+    store_accessor :votes, vote_topic
+    validates vote_topic, presence: true
+  end
 end
