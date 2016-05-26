6b685a86e9fe99ec5cbb01d74bd8e2548663d2ae
Alexis Reigel <mail@koffeinfrei.org>
2016-05-26 11:54:12 +0200

    feat(rating): fill up remaining empty stars

--- spec/helpers/events_helper_spec.rb
+++ spec/helpers/events_helper_spec.rb
@@ -6,12 +6,20 @@ RSpec.describe EventsHelper do
       expect(helper.vote_as_stars(2.5)).to eq(
         '<i class="material-icons">star</i>' \
         '<i class="material-icons">star</i>' \
-        '<i class="material-icons">star_half</i>'
+        '<i class="material-icons">star_half</i>' \
+        '<i class="material-icons">star_border</i>' \
+        '<i class="material-icons">star_border</i>'
       )
     end
 
     it 'returns full star for non-half votes' do
-      expect(helper.vote_as_stars(1)).to eq '<i class="material-icons">star</i>'
+      expect(helper.vote_as_stars(1)).to eq(
+        '<i class="material-icons">star</i>' \
+        '<i class="material-icons">star_border</i>' \
+        '<i class="material-icons">star_border</i>' \
+        '<i class="material-icons">star_border</i>' \
+        '<i class="material-icons">star_border</i>'
+      )
     end
   end
 end
