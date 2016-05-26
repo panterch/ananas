4615e43635e5baa1c14deaef3fbb80477a8f4342
Markus Koller <markus-koller@gmx.ch>
2016-05-26 12:29:10 +0300

    feat(calendars): Add ICS calendar for events

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+describe User, type: :model do
+
+  describe '.find_by_calendar_token!' do
+    let!(:user) { create :user, id: 23 }
+
+    it 'raises an exception for unknown users' do
+      expect do
+        User.find_by_calendar_token! "#{user.calendar_token[0...40]}666"
+      end.to raise_error(ActiveRecord::RecordNotFound)
+    end
+
+    it 'raises an exception for invalid tokens' do
+      expect do
+        User.find_by_calendar_token! '5d81f9b3e7742d10ae6907bd9c906f0d82ecf41123'
+      end.to raise_error(ActiveRecord::RecordNotFound)
+    end
+
+    it 'does not raise an exception for a valid token' do
+      expect do
+        User.find_by_calendar_token! user.calendar_token
+      end.not_to raise_error
+    end
+  end
+
+  describe '#calendar_token' do
+    let(:user) { build :user, id: '23', encrypted_password: 'secret' }
+
+    it 'generates a hash and appends the ID' do
+      expect(user.calendar_token).to eq 'c0c7467cc203bd328ce23389a6921e3d233d8e1923'
+    end
+
+    it 'generates a different hash when the password changes' do
+      token = user.calendar_token
+      user.encrypted_password = 'changed'
+
+      expect(user.calendar_token).not_to eq token
+    end
+  end
+end
