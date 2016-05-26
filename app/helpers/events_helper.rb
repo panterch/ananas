98ad6fbcf08094d53568999116e529411db80e79
seb <seb@panter.ch>
2016-05-26 20:03:28 +0300

    chore(design): styled timeline

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,10 +1,7 @@
 module EventsHelper
   def colors(index)
     # The base color has no additional class
-    colors = ['darken-4', 'darken-3', 'darken-2', 'darken-1', '',
-              'lighten-1', 'lighten-2', 'lighten-3', 'lighten-4',
-              'lighten-5', 'lighten-4', 'lighten-3', 'lighten-2',
-              'lighten-1', '', 'darken-1', 'darken-2', 'darken-3']
+    colors = ['color-1', 'color-2', 'color-3', 'color-4']
     colors[index % colors.length]
   end
 
