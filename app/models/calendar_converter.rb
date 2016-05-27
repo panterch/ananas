adfbe6e940bb532f19186dbe44070514371c9178
Markus Koller <markus-koller@gmx.ch>
2016-05-27 13:52:29 +0300

    feat(calendars): Include event attendances

--- app/models/calendar_converter.rb
+++ app/models/calendar_converter.rb
@@ -8,6 +8,41 @@ module CalendarConverter
       ical.url = event.url
       ical.created = event.created_at
       ical.last_modified = event.updated_at
+
+      event.attendances.each do |attendance|
+        ical.append_attendee self.attendee(attendance)
+      end
     end
+  end
+
+  def self.attendee(attendance)
+    guest = attendance.guest
+    user = guest.user
+
+    uri =
+      if user and user.email.present?
+        "mailto:#{user.email}"
+      else
+        # TODO: use profile URL or something
+        "http://ananas.local"
+      end
+
+    role =
+      if guest.is_a? Mentor
+        'CHAIR'
+      else
+        'REQ-PARTICIPANT'
+      end
+
+    Icalendar::Values::CalAddress.new(uri, {
+      cn: guest.name,
+      cutype: 'INDIVIDUAL',
+      role: role,
+      partstat: {
+        'invited'   => 'TENTATIVE',
+        'attending' => 'ACCEPTED',
+        'declined'  => 'DECLINED',
+      }[attendance.state]
+    })
   end
 end
--- spec/models/calendar_converter_spec.rb
+++ spec/models/calendar_converter_spec.rb
@@ -1,22 +1,29 @@
 require 'rails_helper'
 
 describe CalendarConverter, type: :model do
+  let(:mentor) { build :mentor, name: 'Mentor', user: build(:user, email: 'mentor@example.com') }
+  let(:member) { build :member, name: 'Member', user: build(:user, email: 'member@example.com') }
+
+  let(:event) {
+    Event.new({
+      summary: 'summary',
+      description: 'description',
+      start_at: DateTime.parse('2001-02-03 06:00:00'),
+      end_at: DateTime.parse('2001-02-03 18:00:00'),
+      url: 'url',
+      created_at: DateTime.parse('2001-01-01 12:00:00'),
+      updated_at: DateTime.parse('2001-01-01 13:00:00'),
+      attendances: [
+        build(:attendance, guest: mentor),
+        build(:attendance, guest: member),
+      ]
+    })
+  }
+
   describe '.event' do
-    let(:event) {
-      Event.new({
-        summary: 'summary',
-        description: 'description',
-        start_at: DateTime.parse('2001-02-03 06:00:00'),
-        end_at: DateTime.parse('2001-02-03 18:00:00'),
-        url: 'url',
-        created_at: DateTime.parse('2001-01-01 12:00:00'),
-        updated_at: DateTime.parse('2001-01-01 13:00:00'),
-      })
-    }
+    let(:ical_event) { CalendarConverter.event(event) }
 
     it 'returns an icalendar event' do
-      ical_event = CalendarConverter.event(event)
-
       expect(ical_event.summary).to eq 'summary'
       expect(ical_event.description).to eq 'description'
       expect(ical_event.dtstart.to_s).to eq '2001-02-03T06:00:00+00:00'
@@ -24,6 +31,55 @@ describe CalendarConverter, type: :model do
       expect(ical_event.url.to_s).to eq 'url'
       expect(ical_event.created.to_s).to eq '2001-01-01T12:00:00+00:00'
       expect(ical_event.last_modified.to_s).to eq '2001-01-01T13:00:00+00:00'
+    end
+
+    it 'appends attendances of the event' do
+      expect(ical_event.attendee).to eq [
+        URI.parse('mailto:mentor@example.com'),
+        URI.parse('mailto:member@example.com'),
+      ]
+    end
+  end
+
+  describe '.attendee' do
+    it 'returns an icalendar address for a mentor' do
+      ical_attendee = CalendarConverter.attendee(event.attendances.first)
+
+      expect(ical_attendee).to eq URI.parse('mailto:mentor@example.com')
+      expect(ical_attendee.ical_params['cn']).to eq 'Mentor'
+      expect(ical_attendee.ical_params['cutype']).to eq 'INDIVIDUAL'
+      expect(ical_attendee.ical_params['role']).to eq 'CHAIR'
+      expect(ical_attendee.ical_params['partstat']).to eq 'TENTATIVE'
+    end
+
+    it 'returns an icalendar address for a member' do
+      ical_attendee = CalendarConverter.attendee(event.attendances.second)
+
+      expect(ical_attendee).to eq URI.parse('mailto:member@example.com')
+      expect(ical_attendee.ical_params['cn']).to eq 'Member'
+      expect(ical_attendee.ical_params['cutype']).to eq 'INDIVIDUAL'
+      expect(ical_attendee.ical_params['role']).to eq 'REQ-PARTICIPANT'
+      expect(ical_attendee.ical_params['partstat']).to eq 'TENTATIVE'
+    end
+
+    it 'maps all attendance states' do
+      attendance = event.attendances.first
+
+      attendance.state = :invited
+      expect(CalendarConverter.attendee(attendance).ical_params['partstat']).to eq 'TENTATIVE'
+
+      attendance.state = :attending
+      expect(CalendarConverter.attendee(attendance).ical_params['partstat']).to eq 'ACCEPTED'
+
+      attendance.state = :declined
+      expect(CalendarConverter.attendee(attendance).ical_params['partstat']).to eq 'DECLINED'
+    end
+
+    it 'falls back to some URL for attendances without a known email address' do
+      attendance = event.attendances.first
+      attendance.guest.user = nil
+
+      expect(CalendarConverter.attendee(attendance)).to eq URI.parse('http://ananas.local')
     end
   end
 end
