32c842cd29426d93af0b6b7d22f53ddd84c092e3
seb <seb@panter.ch>
2016-05-28 19:31:33 +0300

    feature(events): automatically generate attendances when creation mentorings

--- app/models/mentoring.rb
+++ app/models/mentoring.rb
@@ -3,11 +3,20 @@ class Mentoring < Event
   belongs_to :mentor
   belongs_to :team
 
+  after_create :create_attendances
+
   def prefill(team)
     self.mentor = team.mentor
     self.summary = "Mentoring #{team.mentor} and #{team}"
     self.start_at = Time.now.beginning_of_hour
     self.end_at = self.start_at + 2.hour
+  end
+
+  def create_attendances
+    attendances.create!(state: :invited, guest: self.mentor)
+    team.members.each do |member|
+      attendances.create!(state: :invited, guest: member)
+    end
   end
 
 end
--- spec/models/mentoring_spec.rb
+++ spec/models/mentoring_spec.rb
@@ -2,16 +2,24 @@ require 'rails_helper'
 
 describe Mentoring, type: :model do
 
-  context '#prefill' do
-    let(:mentor) { create(:mentor ) }
-    let(:team) { create(:team, mentors: [mentor], name: 'T') }
+  let(:mentor) { create(:mentor ) }
+  let(:team) { create(:team, mentors: [mentor], name: 'T') }
 
-    it 'prefills the mentoring' do
+  context '#prefill' do
+
+    it '#prefill' do
       @mentoring = team.mentorings.build()
       @mentoring.prefill(team)
       expect(@mentoring.mentor).to eq(mentor)
       expect(@mentoring.summary).to eq("Mentoring Anonymous Mentor and T")
     end
+  end
 
+  it '#create_attenances' do
+    @member = create(:member, team: team)
+    @mentoring = create(:mentoring, team: team, mentor: mentor)
+    expect(@mentoring.attendances.count).to eq(2)
+    expect(@mentoring.attendances.map(&:guest)).to include(mentor)
+    expect(@mentoring.attendances.map(&:guest)).to include(@member)
   end
 end
