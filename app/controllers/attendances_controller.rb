1b3c02835e69fc49322c7ee3f3edeb5bd9b0bde2
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2016-06-04 18:06:15 +0200

    feat(attendances): allow adding Members and Teams as attendance guests

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,5 +1,37 @@
-class AttendancesController < CrudController
-  belongs_to :event, :mentoring, polymorphic: true, optional: true
+class AttendancesController < ApplicationController
+  authorize_resource
+  respond_to :js, :html
+
+  attr_accessor :parent
+  helper_method :parent
+  before_action do
+    event_id = params[:event_id] || params[:mentoring_id]
+    @parent = Event.find(event_id)
+  end
+
+  attr_accessor :resource
+  helper_method :resource
+  before_action do
+    @resource = @parent.attendances.find(params[:id]) if params[:id]
+  end
+
+  def resource_class
+    Attendance
+  end
+  helper_method :resource_class
+
+  def create
+    attendance_params = params[:attendance]
+
+    result = Attendance.build_with_guest_ident(parent, attendance_params[:guest_ident])
+    if result.is_a? Array
+      result.each(&:save)
+      self.resource = result.first
+    else
+      self.resource = result
+      resource.save
+    end
+  end
 
   def attend
     resource.state = 'attending'
@@ -9,14 +41,5 @@ class AttendancesController < CrudController
   def decline
     resource.state = 'declined'
     resource.save
-  end
-
-  def attendance_params
-    permitted_params = params.require(:attendance)
-
-    permitted_params.permit([
-      :guest_id,
-      :guest_type
-    ])
   end
 end
--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -9,4 +9,22 @@ class Attendance < ActiveRecord::Base
   belongs_to :guest, polymorphic: true
 
   validates_presence_of :event, :guest
+
+  def self.build_with_guest_ident(event, guest_ident)
+    result = self.new(event: event)
+
+    type, ident = guest_ident.split(':')
+    # TODO: check on allowed types
+    if type == 'Team'
+      team = Team.find(ident)
+      result = team.members.map do |member|
+        self.new(event: event, guest: member)
+      end
+    else
+      result.guest_type = type
+      result.guest_id = ident
+    end
+
+    result
+  end
 end
