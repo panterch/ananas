578569952854ec45b13b2c5cde5740237c3c6e9e
Simon Hürlimann (CyT) <simon.huerlimann@cyt.ch>
2016-06-30 23:04:21 +0200

    fix(deploy): configure linked_files

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,7 @@ set :repo_url, 'https://github.com/panterch/ananas.git'
 # set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :linked_files, fetch(:linked_files, []).push('.env', 'public/uploads')
 
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system')
