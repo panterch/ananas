aad5c1b4d089e865707cb8043584afefab1dd2ad
Simon Hürlimann <sim@panter.ch>
2016-07-12 17:32:41 +0200

    test(attendances): fix feature spec

--- spec/features/attendance_stories_spec.rb
+++ spec/features/attendance_stories_spec.rb
@@ -11,7 +11,6 @@ feature "Attendance of Events" do
 
     click_link 'add'
 
-    expect(page).to have_css('.select-wrapper li.optgroup', text: 'Mentors', visible: false)
     find('.select-wrapper input').click #open the dropdown
     find('.select-wrapper li', text: 'Mentor One').click #select the option wanted
 
@@ -21,24 +20,4 @@ feature "Attendance of Events" do
       expect(page).to have_content('Mentor One')
     end
   end
-
-  scenario "Inviting Members to events", js: true do
-    create :member, vcard_attributes: { full_name: "Member One" }
-    event = create :event
-
-    visit event_path(event)
-
-    click_link 'add'
-
-    expect(page).to have_css('.select-wrapper li.optgroup', text: 'Founders', visible: false)
-    find('.select-wrapper input').click #open the dropdown
-    find('.select-wrapper li', text: 'Member One').click #select the option wanted
-
-    click_link 'Save'
-
-    within '.attendances' do
-      expect(page).to have_content('Member One')
-    end
-  end
-
 end
