77330725435cc33328df38a8cb83032daf9f4a07
Simon Hürlimann <sim@panter.ch>
2016-07-27 09:30:31 +0200

    test(teams): fix spec

--- spec/features/startup_creation_spec.rb
+++ spec/features/startup_creation_spec.rb
@@ -12,13 +12,13 @@ feature 'Startup creation' do
     click_link 'add'
     expect(page).not_to have_content 'You are not authorized to access this page'
 
-    fill_in 'team[name]', with: startup_name
     fill_in 'team[description]', with: 'Mjamjam'
+    fill_in 'team[vcard_attributes][full_name]', with: startup_name
     fill_in 'team[vcard_attributes][address_attributes][street_address]', with: 'Aegeristr. 123'
     fill_in 'team[vcard_attributes][address_attributes][postal_code]', with: '6300'
     fill_in 'team[vcard_attributes][address_attributes][locality]', with: 'Zug'
 
-    click_button 'Save'
+    click_button 'Save', match: :first
 
     expect(page).to have_content 'Startup was successfully created'
 
@@ -26,11 +26,10 @@ feature 'Startup creation' do
     find("a[href$='/team_members/new']").click
 
     click_link 'create new member'
-    fill_in 'member[name]', with: 'Alexander Adam'
     fill_in 'member[vcard_attributes][family_name]', with: 'Adam'
     fill_in 'member[vcard_attributes][given_name]', with: 'Alexander'
 
     find('a', text: 'SAVE').click
-    expect(page).to have_content 'Alexander Adam'
+    expect(page).to have_content 'Adam Alexander'
   end
 end
