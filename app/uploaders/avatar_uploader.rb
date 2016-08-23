3112baf0a77ae8cb96e2f5a3417b5e09d248bf0f
Simon Hürlimann <sim@panter.ch>
2016-08-23 13:43:13 +0200

    fix(profiles): use SVG avatar fallback

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -20,7 +20,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # Provide a default URL as a default if there hasn't been a file uploaded:
   def default_url
     # For Rails 3.1+ asset pipeline compatibility:
-    ActionController::Base.helpers.image_path('fallbacks/' + [version_name, 'avatar_default.png'].compact.join('_'))
+    ActionController::Base.helpers.image_path('fallbacks/' + [version_name, 'avatar_default.svg'].compact.join('_'))
   end
 
   # Process files as they are uploaded:
