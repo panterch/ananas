1568df6b3cc8bb04953c3814400e35ec6e389286
Simon Hürlimann <sim@panter.ch>
2016-09-06 16:09:14 +0200

    feat(expert-sessions): add expert session tab to mentor profile

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,6 +1,23 @@
 class MembersController < CrudController
   belongs_to :team, optional: true
 
+  def edit
+    upcoming_sessions = resource.expert_sessions.upcoming
+    @unconfirmed_expert_sessions = upcoming_sessions.merge(Attendance.invited)
+    @accepted_expert_sessions = upcoming_sessions.merge(Attendance.attending)
+    @declined_expert_sessions = upcoming_sessions.merge(Attendance.declined)
+    edit!
+  end
+
+  def show
+    upcoming_sessions = resource.expert_sessions.upcoming
+    @unconfirmed_expert_sessions = upcoming_sessions.merge(Attendance.invited)
+    @accepted_expert_sessions = upcoming_sessions.merge(Attendance.attending)
+    @declined_expert_sessions = upcoming_sessions.merge(Attendance.declined)
+
+    show!
+  end
+
   def member_params
     permitted_params = params.require(:member)
 
